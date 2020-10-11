c5622fb42117ca02c07e9c3487c93acb8e9aabf4
Teofilo Monteiro <teofilo.jose.marques.monteiro@LEGO.com>
2020-10-11 10:21:27 +0200

    Updates

---
+++ precache-manifest.b707061870f3922955998755cf1f8ffe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a04c1b16d7c58290193f4c8383a10d",
+    "url": "/my-home/index.html"
+  },
+  {
+    "revision": "0819b8dabce10efc92c2",
+    "url": "/my-home/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b0c435a3adc3ed696f3",
+    "url": "/my-home/static/js/2.a3096587.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-home/static/js/2.a3096587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0819b8dabce10efc92c2",
+    "url": "/my-home/static/js/main.19ebf92d.chunk.js"
+  },
+  {
+    "revision": "aed9109603a0bb93386c",
+    "url": "/my-home/static/js/runtime-main.b57a1af9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-home/static/media/logo.5d5d9eef.svg"
+  }
+]);
