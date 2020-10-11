cc631447e21568eb4a4aac142bfe708cf23b4fac
Teofilo Monteiro <teofilo.jose.marques.monteiro@LEGO.com>
2020-10-11 18:42:56 +0200

    Refactor Language selector to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,16 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Language from './component/Language'
 
 // import logo from './logo.svg';
 import './App.css';
 
-let defaultLanguage = "en";
-
 function App() {
-  const { t, i18n } = useTranslation("welcome");
-
-  let history = useHistory();
-  let { search } = useLocation();
-
-  let params = new URLSearchParams(search);
-  const currentLng = params.get("lng") || defaultLanguage;
-
-  useEffect(() => {
-    if (value !== params.get("lng")) {
-      history.push(`?lng=${value}`);
-      i18n.changeLanguage(value);
-    }
-  });
-
-  const [value, setValue] = useState(currentLng);
-
-  function handleSelect(e: ChangeEvent<HTMLSelectElement>) {
-    setValue(e.target.value);
-  }
+  const { t } = useTranslation("welcome");
 
   return (
     <div className="App">
-      <div className="App-language">
-        <div>{t('language')}</div>
-        <select value={value} onChange={handleSelect}>
-          <option value="en">en</option>
-          <option value="pt">pt</option>
-        </select>
-
-      </div>
-
+      <Language />
 
       <header className="App-header">
 
